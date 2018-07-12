2eeb00137c85e65cf1480d9edfa59f7be503905a
Logan Lee <logan.jazzu@gmail.com>
2018-07-12 13:31:54 -0600

    added a small thank you footer

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -143,7 +143,7 @@ class Main extends Component {
             <hr />
             <p className="pull-right">
               <i className="fa fa-github" aria-hidden="true"></i>
-              Proudly built using React.js
+              Thank you Scott and Jonathan for your help.
             </p>
           </footer>
         </div>
